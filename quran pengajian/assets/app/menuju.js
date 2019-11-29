48665b611f7d6af6617bda74c4b9c3451acd26d1
mzaini30 <muhzaini30@gmail.com>
2019-11-29 19:55:20 +0800

    oke

--- quran pengajian/assets/app/menuju.js
+++ quran pengajian/assets/app/menuju.js
@@ -1,5 +1,5 @@
 $(function(){
-	if (location.search){
+	if (location.search.match(/\?bagian_.+_.+/)){
 		arah = location.search.split('?')
 
 		$('html, body').animate({
@@ -7,14 +7,25 @@ $(function(){
 		}, 800)
 	}
 
+	if (location.search.match(/\?posisi_.+/)){
+		posisi = location.search.replace('?posisi_', '')
+		$('html, body').animate({
+			scrollTop: posisi
+		}, 800)	
+	}
+
 	if (typeof beranda == 'undefined'){
 		simpan_posisi = function(){
 			localStorage.setItem('posisi', $('html, body').scrollTop())
-			localStorage.setItem('file', location.href.substring(location.href.lastIndexOf('/') + 1))
+			localStorage.setItem('file', location.href.substring(location.href.lastIndexOf('/') + 1).replace(/\?bagian_.+_.+/g, '').replace(/\?posisi_.+/g, ''))
 		}
 		simpan_posisi()
 		$(window).scroll(function(){
 			simpan_posisi()
 		})
+	}
+
+	if (localStorage.getItem('posisi')){
+		$('.halaman-terakhir').attr('href', localStorage.getItem('file') + '?posisi_' + localStorage.getItem('posisi'))
 	}
 })
