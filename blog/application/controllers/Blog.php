58e44611f77a8fad123a11f976e09b6e8babedee
mzaini30 <muhzaini30@gmail.com>
2019-12-16 22:51:27 +0800

    oke

--- blog/application/controllers/Blog.php
+++ blog/application/controllers/Blog.php
@@ -5,4 +5,12 @@ class Blog extends MY_Controller {
 	public function index(){
 		$this->twig->display('blog/beranda');
 	}
+
+	public function tambah(){
+		if (!$_POST){
+			$this->twig->display('blog/tambah');
+		} else {
+
+		}
+	}
 }
