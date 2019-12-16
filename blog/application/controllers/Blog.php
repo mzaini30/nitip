14e1b8e01fbb5ebab74fe9cf190db3e74431ee7b
mzaini30 <muhzaini30@gmail.com>
2019-12-16 23:08:36 +0800

    oke

--- blog/application/controllers/Blog.php
+++ blog/application/controllers/Blog.php
@@ -3,14 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Blog extends MY_Controller {
 	public function index(){
-		$this->twig->display('blog/beranda');
+		$data = $this->db->order_by('id', 'desc')->get('blog')->result();
+		$this->twig->display('blog/beranda', $data);
 	}
 
 	public function tambah(){
 		if (!$_POST){
 			$this->twig->display('blog/tambah');
 		} else {
-
+			$data = (object) $this->input->post();
+			$data->tanggal = date('d/m/Y H:i:s');
+			$this->db->insert('blog', $data);
+			redirect(site_url());
 		}
 	}
 }
